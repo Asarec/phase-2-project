15b63e3af6918ef3600572160552008014ce1b53
Alfredo Garcia <Asarec@users.noreply.github.com>
2023-02-14 19:45:23 -0500

    removed unused function

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -24,10 +24,6 @@ function LoginForm({ handleIsLoggedIn }) {
     }
   }
 
-  function handleErrorMessage() {
-    setError(error => !error);
-  }
-
   return (
     <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
