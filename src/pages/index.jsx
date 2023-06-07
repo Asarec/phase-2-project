220c12654d3c403d31484098b00745f76a048a19
Alfredo Garcia <Asarec@users.noreply.github.com>
2023-06-07 00:09:39 -0400

    added new page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
 export { default as InvoiceList } from "./InvoiceList";
 export { default as InvoiceDetails } from "./InvoiceDetails";
+export { default as AddInvoice } from "./AddInvoice";
 export { default as FourOhFour } from "./FourOhFour";
