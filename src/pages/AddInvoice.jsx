83902fe464322b82c5d10858121fe31f49f6bce2
Alfredo Garcia <Asarec@users.noreply.github.com>
2023-06-09 02:38:13 -0400

    added control to the form with state management

--- src/pages/AddInvoice.jsx
+++ src/pages/AddInvoice.jsx
@@ -1,34 +1,149 @@
+import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 
 function AddInvoice({ handleInvoicesUpdate }) {
+  const [recipient, setRecipient] = useState({
+    Content: "",
+    formCheck: false,
+  });
+  const [invoiceName, setInvoiceName] = useState({
+    Content: "",
+    formCheck: false,
+  });
+  const [invoiceDescription, setInvoiceDescription] = useState({
+    Content: "",
+    formCheck: false,
+  });
+  const [invoiceAmount, setInvoiceAmount] = useState({
+    Content: "",
+    formCheck: false,
+  });
+  const [invoiceTax, setInvoiceTax] = useState({
+    Content: "",
+    formCheck: false,
+  });
+
+  const formClasses =
+    "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6";
+  const formIndicator = {
+    Good: "ring-gray-300",
+    Bad: "ring-red-300",
+  };
+
   const invoiceNumber = crypto.randomUUID();
   const navigate = useNavigate();
 
-  function formSubmission(event) {
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+  function handleRecipientUpdate(event) {
     event.preventDefault();
+    setRecipient((recipient) => ({
+      ...recipient,
+      Content: event.target.value,
+    }));
+
+    event.target.value.length <= 0
+      ? setRecipient((recipient) => ({ ...recipient, formCheck: true }))
+      : setRecipient((recipient) => ({ ...recipient, formCheck: false }));
+  }
+
+  function handleInvoiceNameUpdate(event) {
+    event.preventDefault();
+    setInvoiceName((invoiceName) => ({
+      ...invoiceName,
+      Content: event.target.value,
+    }));
+
+    event.target.value.length <= 0
+      ? setInvoiceName((invoiceName) => ({ ...invoiceName, formCheck: true }))
+      : setInvoiceName((invoiceName) => ({ ...invoiceName, formCheck: false }));
+  }
+
+  function handleInvoiceDescriptionUpdate(event) {
+    event.preventDefault();
+    setInvoiceDescription((invoiceDescription) => ({
+      ...invoiceDescription,
+      Content: event.target.value,
+    }));
+
+    event.target.value.length <= 0
+      ? setInvoiceDescription((invoiceDescription) => ({ ...invoiceDescription, formCheck: true }))
+      : setInvoiceDescription((invoiceDescription) => ({ ...invoiceDescription, formCheck: false }));
+  }
+
+  function handleInvoiceAmountUpdate(event) {
+    event.preventDefault();
+    setInvoiceAmount((invoiceAmount) => ({
+      ...invoiceAmount,
+      Content: event.target.value,
+    }));
+
+    event.target.value.length <= 0
+      ? setInvoiceAmount((invoiceAmount) => ({ ...invoiceAmount, formCheck: true }))
+      : setInvoiceAmount((invoiceAmount) => ({ ...invoiceAmount, formCheck: false }));
+  }
+
+  function handleInvoiceTaxUpdate(event) {
+    event.preventDefault();
+    setInvoiceTax((invoiceTax) => ({
+      ...invoiceTax,
+      Content: event.target.value,
+    }));
+
+    event.target.value.length <= 0
+      ? setInvoiceTax((invoiceTax) => ({ ...invoiceTax, formCheck: true }))
+      : setInvoiceTax((invoiceTax) => ({ ...invoiceTax, formCheck: false }));
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    let formCheck = false;
 
     const newInvoiceData = {
       reference: invoiceNumber,
       paymentStatus: "Not Paid",
-      billedTo: event.target[0].value,
-      name: event.target[1].value,
-      description: event.target[2].value,
-      dueAmount: Number(event.target[3].value),
-      taxRate: Number(event.target[4].value),
+      billedTo: recipient.Content,
+      name: invoiceName.Content,
+      description: invoiceDescription.Content,
+      dueAmount: Number(invoiceAmount.Content),
+      taxRate: Number(invoiceTax.Content),
     };
 
-    fetch("http://localhost:3000/invoices", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newInvoiceData),
-    })
-      .then((response) => response.json())
-      .then((data) => handleInvoicesUpdate(data));
+    switch (true) {
+      case recipient.Content.length <= 0:
+        setRecipient((recipient) => ({ ...recipient, formCheck: true }));
+        formCheck = true;
+      case invoiceName.Content.length <= 0:
+        setInvoiceName((invoiceName) => ({ ...invoiceName, formCheck: true }));
+        formCheck = true;
+      case invoiceDescription.Content.length <= 0:
+        setInvoiceDescription((invoiceDescription) => ({ ...invoiceDescription, formCheck: true }));
+        formCheck = true;
+      case invoiceAmount.Content.length <= 0 || isNaN(invoiceAmount.Content):
+        setInvoiceAmount((invoiceAmount) => ({ ...invoiceAmount, formCheck: true }));
+        formCheck = true;
+      case invoiceTax.Content.length <= 0 || isNaN(invoiceTax.Content):
+        setInvoiceTax((invoiceTax) => ({ ...invoiceTax, formCheck: true }));
+        formCheck = true;
+    }
 
-    navigate("/");
+    if (formCheck === false) {
+      fetch("http://localhost:3000/invoices", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newInvoiceData),
+      })
+        .then((response) => response.json())
+        .then((data) => handleInvoicesUpdate(data));
+
+      navigate("/");
+    }
   }
 
   return (
@@ -44,29 +159,32 @@ function AddInvoice({ handleInvoicesUpdate }) {
           <p className="text-xs leading-8 text-gray-300">{invoiceNumber}</p>
         </div>
         <div className="px-5 pt-5">
-          <form onSubmit={(event) => formSubmission(event)}>
+          <form>
             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
               Client Email
             </label>
             <div className="mt-2">
               <input
                 type="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className={classNames(formIndicator[`${recipient.formCheck ? "Bad" : "Good"}`], formClasses)}
                 placeholder="client@company.com"
+                onChange={(event) => handleRecipientUpdate(event)}
               />
             </div>
             <label className="mt-6 block text-sm font-medium leading-6 text-gray-900">Invoice Name</label>
             <div className="mt-2">
               <input
                 type="text"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className={classNames(formIndicator[`${invoiceName.formCheck ? "Bad" : "Good"}`], formClasses)}
+                onChange={(event) => handleInvoiceNameUpdate(event)}
               />
             </div>
             <label className="mt-6 block text-sm font-medium leading-6 text-gray-900">Invoice Description</label>
             <div className="mt-2">
               <textarea
                 rows={4}
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className={classNames(formIndicator[`${invoiceDescription.formCheck ? "Bad" : "Good"}`], formClasses)}
+                onChange={(event) => handleInvoiceDescriptionUpdate(event)}
               />
             </div>
             <div className="flex justify-between">
@@ -78,7 +196,11 @@ function AddInvoice({ handleInvoicesUpdate }) {
                   </div>
                   <input
                     type="text"
-                    className="block w-full rounded-md border-0 py-1.5 pl-7 pr-12 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className={classNames(
+                      formIndicator[`${invoiceAmount.formCheck ? "Bad" : "Good"}`],
+                      formClasses + " pl-7 pr-12"
+                    )}
+                    onChange={(event) => handleInvoiceAmountUpdate(event)}
                     placeholder="0.00"
                   />
                   <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
@@ -91,7 +213,8 @@ function AddInvoice({ handleInvoicesUpdate }) {
                 <div className="relative mt-2">
                   <input
                     type="text"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className={classNames(formIndicator[`${invoiceTax.formCheck ? "Bad" : "Good"}`], formClasses)}
+                    onChange={(event) => handleInvoiceTaxUpdate(event)}
                     placeholder="0"
                   />
                   <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
@@ -103,6 +226,7 @@ function AddInvoice({ handleInvoicesUpdate }) {
             <button
               type="submit"
               className="mt-6 w-full rounded bg-indigo-600 px-2 py-1 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={(event) => handleFormSubmit(event)}
             >
               Submit
             </button>
